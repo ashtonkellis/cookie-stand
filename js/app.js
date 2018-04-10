62a6e1ce3197ef7306c652268aa196424814d9e3
Ashton K Ellis <ashtonkellis@gmail.com>
2018-04-10 15:44:07 -0700

    render table header function done

--- js/app.js
+++ js/app.js
@@ -4,8 +4,24 @@ var storeHours = ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm'
 
 var stores = [];
 
+var salesTableHead = document.getElementById('sales-table-head');
+var salesTableBody = document.getElementById('sales-table-body');
+var salesTableFoot = document.getElementById('sales-table-foot');
+
 function randIntBetween(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function addTH(elementText) {
+  var thEL = document.createElement('th');
+  thEL.textContent = elementText;
+  return thEL;
+}
+
+function addTD() {
+  var tdEL = document.createElement('td');
+  tdEL.textContent = elementText;
+  return tdEL;
 }
 
 var Store = function(storeName, storeId, hourlyCustomersMin, hourlyCustomersMax, avgCookiesPerCustomer) {
@@ -55,27 +71,27 @@ Store.prototype.estimateAll = function() {
   this.estimateCustomers();
   this.estimateCookies();
   this.estimateTotalCookies();
-},
+};
 
-Store.prototype.renderHourlyEstimates = function() {
-  var ulEL = document.getElementById(this.storeId);
-  for (var i = 0; i < this.cookiesEachHour.length; i++) {
-    var liEL = document.createElement('li');
-    liEL.textContent = storeHours[i] + ': ' + this.cookiesEachHour[i];
-    ulEL.appendChild(liEL);
+Store.renderTableHeader = function () {
+  // create table row
+  var trEL = document.createElement('tr');
+  var thEL;
+
+  // create first th element (blank cell) and append to tr element
+  trEL.appendChild(addTH(''));
+
+  // loop through all store locations and append store names to tr
+  for (var i in stores) {
+    var storeName = stores[i].storeName;
+    trEL.appendChild(addTH(storeName));
   }
-};
 
-Store.prototype.renderTotalEstimate = function() {
-  var ulEL = document.getElementById(this.storeId);
-  var liEL = document.createElement('li');
-  liEL.textContent = 'Total: ' + this.totalCookies;
-  ulEL.appendChild(liEL);
-};
+  // create last th element (total's column) and append to tr
+  trEL.appendChild(addTH('Daily Location Total'));
 
-Store.prototype.renderAllEstimates = function() {
-  this.renderHourlyEstimates();
-  this.renderTotalEstimate();
+  // append table row to table
+  salesTableHead.appendChild(trEL);
 };
 
 var firstAndPike = new Store('1st and Pike', 'firstAndPike', 23, 65, 6.3);
@@ -83,11 +99,3 @@ var seaTac = new Store('SeaTac Airport', 'seaTac', 3, 24, 1.2);
 var seattleCenter = new Store('Seattle Center', 'seattleCenter', 11, 38, 3.7);
 var capitolHill = new Store('Capitol Hill', 'capitolHill', 20, 38, 2.3);
 var alki = new Store('Alki', 'alki', 2, 16, 4.6);
-
-Store.renderAllEstimates = function () {
-  for (var i in stores) {
-    stores[i].renderAllEstimates();
-  }
-};
-
-Store.renderAllEstimates();
